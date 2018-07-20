980c1a7a54fc73effa87f75d1ea422abaeed06a1
David Kotlirevsky <david.kotlirevsky@gmail.com>
2018-07-20 13:32:42 -0300

    Adding some angular updates

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private serverUrl = 'http://localhost:8080/api/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/api/chat", (message) => {
+        if(message.body) {
+          debugger
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message) {
+    this.stompClient.send("/api/app/send/message", {}, message);
+  }
+
 }
